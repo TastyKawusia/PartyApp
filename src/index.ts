ebdb7e1ca630635266267ba44b716a3ae844d621
Jasio <jasio182016@gmail.com>
2023-03-04 17:31:31 +0100

    For check 2

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-
+import routes from './user.routing';
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
-
 app.get('/', (req: Request, res: Response) => {
   res.send('Server is running');
 });
-
+routes.use("/api/user",)  // Something like that? How to connect user.routing with index
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
